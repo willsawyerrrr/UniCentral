f9d78d7d7bb0473484426294142a920246ab0e70
willsawyer2609 <wmsawyer2609@gmail.com>
2022-05-23 19:54:01 +1000

    Implemented URL-dependent navigation path

--- js/path.js
+++ js/path.js
@@ -1,12 +1,35 @@
-var path = document.getElementById("path");
-let dirs = window.location.pathname.split("/");
+onload=setPath;
 
-for (directory of dirs) {
-    let caret = document.createElement("p");
-    caret.innerText = "&rsaquo;";
-    path.appendChild(caret);
+function setPath() {
+    var path = document.getElementById("path");
+    let dirs = window.location.pathname.split("/");
 
-    let child = document.createElement("a");
-    child.innerText = directory;
-    path.appendChild(child);
+    if (dirs.length == 1) return;
+
+    for (subdir of dirs) {
+        if (!subdir) {
+            let child = document.createElement("a");
+            child.setAttribute("href", "index.html");
+            path.appendChild(child);
+            
+            let image = document.createElement("img");
+            image.setAttribute("src", "images/home.jpg");
+            image.setAttribute("alt", "Home icon");
+            child.appendChild(image);
+            
+            continue;
+        }
+
+        let caret = document.createElement("p");
+        caret.innerHTML = "&rsaquo;";
+        path.appendChild(caret);
+
+        let child = document.createElement("a");
+        child.innerText = cleanSubdir(subdir);
+        path.appendChild(child);
+    }
+}
+
+function cleanSubdir(subdir) {
+    return subdir;
 }
