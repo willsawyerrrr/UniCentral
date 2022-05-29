20f941adc8e2aa72e21ac788e5f48b71929a4af1
willsawyer2609 <wmsawyer2609@gmail.com>
2022-05-29 21:31:13 +1000

    Removed unnecessary media listener

--- js/menu.js
+++ js/menu.js
@@ -1,34 +1,3 @@
-let headerMedia = window.matchMedia("(max-width: 1000px)");
-headerMedia.addListener(setMenu);
-
-/* Shows or hides the navbar menu according to the screen width. */
-function setMenu(media) {
-    if (media.matches) {
-        hideMenu();
-    } else {
-        hideDropdown();
-        showMenu();
-    }
-}
-
-/* Shows the navbar menu. */
-function showMenu() {
-    let menuItems = document.getElementsByClassName("menu");
-    
-    for (element of menuItems) {
-        element.style.display = "block";
-    }
-}
-
-/* Hides the navbar menu. */
-function hideMenu() {
-    let menuItems = document.getElementsByClassName("menu");
-    
-    for (element of menuItems) {
-        element.style.display = "none";
-    }
-}
-
 /* Shows the dropdown menu. */
 function showDropdown() {
     document.getElementById("dropdown").style.display = "flex";
