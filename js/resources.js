26955cec38e112aaba2f2bb31b18f02400a06fa4
willsawyer2609 <wmsawyer2609@gmail.com>
2022-05-30 02:20:27 +1000

    Implemented DECO1400 resources page

--- js/resources.js
+++ js/resources.js
@@ -1,3 +1,35 @@
 addLoadEvent(function() {
     makePath("Resources");
 });
+addLoadEvent(setIcons);
+
+/* Expands to show the relevant list of links/materials. */
+function expand(caller) {
+    caller.onclick = function() {collapse(this)};
+    let container = caller.parentElement.getElementsByTagName("ul")[0];
+    container.style.display = "flex";
+
+    let icon = caller.getElementsByClassName("icon")[0];
+    icon.innerHTML = "&minus;";
+}
+
+function collapse(caller) {
+    caller.onclick = function() {expand(this)};
+    let container = caller.parentElement.getElementsByTagName("ul")[0];
+    container.style.display = "none";
+
+    let icon = caller.getElementsByClassName("icon")[0];
+    icon.innerHTML = "&plus;";
+}
+
+function setIcons() {
+    let files = document.getElementsByClassName("file");
+    for (let file of files) {
+        file.src = "images/paperclip.png";
+    }
+
+    let links = document.getElementsByClassName("link");
+    for (let link of links) {
+        link.src = "images/link.svg";
+    }
+}
